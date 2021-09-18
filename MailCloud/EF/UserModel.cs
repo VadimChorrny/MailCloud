d7c87e65a98408fa4228c43ea1b28ac39816f050
VadimChorrny <chorrny228@gmail.com>
2021-09-18 14:15:05 +0300

    Add project files.

---
+++ MailCloud/EF/UserModel.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MailCloud.EF
+{
+    public class UserModel : DbContext
+    {
+        public UserModel()
+            : base("name=UserModel")
+        {
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Message> Messages { get; set; }
+    }
+
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Message
+    {
+        public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Theme { get; set; }
+        public string Body { get; set; }
+        public int? AccountId { get; set; }
+        public Account Account { get; set; }
+    }
+}
